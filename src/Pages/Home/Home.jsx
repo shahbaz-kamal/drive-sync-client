05097b803822d6b3175067af8b71201748d53151
shahbaz-kamal <shahbazkamal384@gmail.com>
2025-03-28 21:34:27 +0600

    added context provider successfully

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import Banner from "./Banner";
 import UploadForm from "./UploadForm";
+import UseAuth from "../../Hooks/UseAuth";
 
 const Home = () => {
+    const name=UseAuth()
+    console.log(name)
   return (
     <div className="space-y-10">
       <Banner></Banner>
---
+++ src/Provider/AuthProvider.jsx
@@ -0,0 +1,10 @@
+import React, { createContext } from "react";
+
+export const AuthContext = createContext(null);
+
+const AuthProvider = ({ children }) => {
+  const authInfo = { name: "Tamim" };
+  return <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>;
+};
+
+export default AuthProvider;
